fdd645c8024438e10b6779c3055dc96414beca6b
Meeple397 <islaymaker397@gmail.com>
2025-02-12 14:43:10 -0500

    word count added to main

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     print(file_contents)
+    words = file_contents.split()
+    word_count = 0
+    for s in range(0, len(words)):
+        word_count += 1
+    print(word_count)
 
 main()
